6a2ac8ec7c4a0dfa7ec54b2fa779dd26be369092
Ginezis <Ginezis25@gmail.com>
2023-07-03 02:30:48 +0300

    feat: expand basic functionality, add 3 new levels

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,16 +1,20 @@
 import levels from "./levels"
 
 class CSSDinnerApp {
+  game: HTMLElement | null
   levelNumber: number
   levels
   title: HTMLElement
-  helpButton: Element | null
-  table: Element | null
-  input: HTMLInputElement | null
-  enterButton: Element | null
-  htmlField: Element | null
+  helpButton: HTMLElement | null
+  table: HTMLElement
+  input: HTMLInputElement
+  enterButton: HTMLElement | null
+  htmlField: HTMLElement
   curentLevel: HTMLElement
   maxLevels: HTMLElement
+  levelState: HTMLElement | null
+  prevLvlButton: HTMLElement | null
+  nextLvlButton: HTMLElement | null
   helpTitle: HTMLElement
   helpSubTitle: HTMLElement
   helpSyntax: HTMLElement
@@ -18,16 +22,20 @@ class CSSDinnerApp {
   helpList: HTMLElement
 
   constructor(levelNumber: number) {
+    this.game = document.querySelector('.css-diner__game')
     this.levelNumber = levelNumber
     this.levels = levels
     this.title = document.querySelector('.css-diner__task') as HTMLElement
     this.helpButton = document.querySelector('.css-diner__help-btn')
-    this.table = document.querySelector('.css-diner__table')
-    this.input = document.querySelector('.css-diner__input')
+    this.table = document.querySelector('.css-diner__table') as HTMLElement
+    this.input = document.querySelector('.css-diner__input') as HTMLInputElement
     this.enterButton = document.querySelector('.css-diner__enter-btn')
-    this.htmlField = document.querySelector('.css-diner__html-field')
+    this.htmlField = document.querySelector('.css-diner__html-field') as HTMLElement
     this.curentLevel = document.querySelector('.help__curent-level') as HTMLElement
     this.maxLevels = document.querySelector('.help__max-levels') as HTMLElement
+    this.levelState = document.querySelector('.help__level-state')
+    this.prevLvlButton = document.querySelector('.help__prev-level')
+    this.nextLvlButton = document.querySelector('.help__next-level')
     this.helpTitle = document.querySelector('.help__title') as HTMLElement
     this.helpSubTitle = document.querySelector('.help__subtitle') as HTMLElement
     this.helpSyntax = document.querySelector('.help__syntax') as HTMLElement
@@ -36,29 +44,39 @@ class CSSDinnerApp {
   }
 
   public start(): void {
-    // this.test()
     this.renderLevel(this.levelNumber)
-
+    this.game?.addEventListener('mouseover', this.hover)
+    this.input?.addEventListener('input', this.inputState)
     this.helpButton?.addEventListener('click', () => {
-      console.log('help')
+      console.log(this.levels[this.levelNumber].correctAnswer)
     })
-
-    this.enterButton?.addEventListener('click', () => {
-      if (this.input) {
-        console.log(this.input.value)
-      }
-    })
+    this.enterButton?.addEventListener('click', this.isCorrectAnswer)
+    this.prevLvlButton?.addEventListener('click', this.renderPrevLevel)
+    this.nextLvlButton?.addEventListener('click', this.renderNextLevel)
   }
 
   private renderLevel(levelNumber: number): void {
     this.title.textContent = this.levels[levelNumber].title
-    this.helpTitle.textContent = this.levels[levelNumber].helpTitle
+    this.input.value = ''
+    this.input.classList.add('css-diner__input_strobe')
+    this.htmlField.innerHTML = this.levels[levelNumber].htmlField
+    this.table.innerHTML = this.levels[levelNumber].table
     this.curentLevel.textContent = `${this.levels[levelNumber].curentLevel}`
     this.maxLevels.textContent = `${this.levels.length}`
+    this.renderLevelState()
+    this.helpTitle.textContent = this.levels[levelNumber].helpTitle
     this.helpSubTitle.textContent = this.levels[levelNumber].helpSubTitle
     this.helpSyntax.textContent = this.levels[levelNumber].helpSyntax
     this.helpPromt.innerHTML = this.levels[levelNumber].helpPromt
     this.renderHelpList(this.levels[levelNumber].helpList)
+  }
+
+  private renderLevelState(): void {
+    if (this.levels[this.levelNumber].isDone === true) {
+      this.levelState?.classList.add('_done')
+    } else {
+      this.levelState?.classList.remove('_done')
+    }
   }
 
   private renderHelpList(helpList: string[]): void {
@@ -71,19 +89,65 @@ class CSSDinnerApp {
     })
   }
 
-  private test(): void {
-    console.log(this.title)
-    console.log(this.helpButton)
-    console.log(this.table)
-    console.log(this.input)
-    console.log(this.enterButton)
-    console.log(this.htmlField)
-    console.log(this.helpTitle)
-    console.log(this.helpSubTitle)
-    console.log(this.helpSyntax)
-    console.log(this.helpPromt)
-    console.log(this.helpList)
-    console.log(this.levels)
+  private hover(e: MouseEvent): void {
+    const target: HTMLElement = e.target as HTMLElement
+    if (target.hasAttribute('data-tag')) {
+      const tagValue = target.getAttribute('data-tag')
+      const elements = document.querySelectorAll(`[data-tag = ${tagValue} ]`)
+
+      elements.forEach(element => {
+        element.classList.add('_hover')
+      })
+    }
+    if (!target.hasAttribute('data-tag')) {
+      const tagValue = target.getAttribute('data-tag')
+      const elements = document.querySelectorAll(`[data-tag]`)
+      elements.forEach(element => {
+        element.classList.remove('_hover')
+      })
+    }
+  }
+
+  private inputState = (): void => {
+    if (this.input?.value) {
+      this.input?.classList.remove('css-diner__input_strobe')
+    } else { this.input?.classList.add('css-diner__input_strobe') }
+  }
+
+  private isCorrectAnswer = (e: Event): void => {
+    e.preventDefault()
+    if (this.input?.value.trim() === this.levels[this.levelNumber].correctAnswer) {
+      this.levelState?.classList.add('_done')
+      this.levels[this.levelNumber].isDone = true
+      this.renderNextLevel()
+
+      this.renderLevel(this.levelNumber)
+
+    } else {
+      console.log(false)
+    }
+  }
+
+  private renderPrevLevel = () => {
+    if (!this.prevLvlButton?.classList.contains('_active')) return
+    if (!this.nextLvlButton?.classList.contains('_active')) this.nextLvlButton?.classList.add('_active')
+    this.levelNumber -= 1
+    this.renderLevel(this.levelNumber)
+    if (this.levelNumber === 0) {
+      this.prevLvlButton?.classList.remove('_active')
+    }
+  }
+
+  private renderNextLevel = () => {
+    if (!this.nextLvlButton?.classList.contains('_active')) return
+    if (!this.prevLvlButton?.classList.contains('_active')) this.prevLvlButton?.classList.add('_active')
+
+    this.levelNumber += 1
+    this.renderLevel(this.levelNumber)
+
+    if (this.levelNumber + 1 >= this.levels.length) {
+      this.nextLvlButton?.classList.remove('_active')
+    }
   }
 }
 
--- src/scripts/levels.ts
+++ src/scripts/levels.ts
@@ -14,9 +14,9 @@ interface Level {
 
 const levels: Level[] = [
   {
-    title: 'Select the bento boxes',
-    table: 'string',
-    htmlField: 'string',
+    title: 'Select the plates',
+    table: '<div data-tag="plate1" class="plate strobe"></div> <div data-tag="plate2" class="plate strobe"></div>',
+    htmlField: '<div data-tag="plate1">&nbsp;&nbsp;&lt;plate/&gt;</div> <div data-tag="plate2">&nbsp;&nbsp;&lt;plate/&gt;</div>',
     curentLevel: 1,
     isDone: false,
     helpTitle: 'Type Selector',
@@ -26,11 +26,10 @@ const levels: Level[] = [
     helpList: ['<strong>div</strong> selects all <tag>div</tag> elements.', '<strong>p</strong> selects all <tag>p</tag> elements.'],
     correctAnswer: 'plate'
   },
-
   {
     title: 'Select the bento boxes',
-    table: 'string',
-    htmlField: 'string',
+    table: '<div data-tag="bento1" class="bento strobe"></div> <div data-tag="plate1" class="plate"></div> <div data-tag="bento2" class="bento strobe"></div>',
+    htmlField: '<div data-tag="bento1">&nbsp;&nbsp;&lt;bento/&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate/&gt;</div> <div data-tag="bento2">&nbsp;&nbsp;&lt;bento/&gt;</div>',
     curentLevel: 2,
     isDone: false,
     helpTitle: 'Type Selector',
@@ -38,22 +37,25 @@ const levels: Level[] = [
     helpSyntax: 'A',
     helpPromt: 'Selects all elements of type <strong>A</strong>.Type refers to the type of tag, so <tag>div</tag>, <tag> p</tag> and <tag>ul</tag> are all different element types.',
     helpList: ['<strong>div</strong> selects all <tag>div</tag> elements.', '<strong>p</strong> selects all <tag>p</tag> elements.'],
-    correctAnswer: 'plate'
+    correctAnswer: 'bento'
   },
   {
-    title: 'Select the bento boxes',
-    table: 'string',
-    htmlField: 'string',
+    title: 'Select the fancy plate',
+    table: '<div data-tag="fancy1" class="fancy strobe"></div> <div data-tag="plate1" class="plate"></div> <div data-tag="bento1" class="bento"></div>',
+    htmlField: '<div data-tag="fancy1">&nbsp;&nbsp;&lt;plate id="fancy" /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate/&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento/&gt;</div>',
     curentLevel: 3,
     isDone: false,
-    helpTitle: 'Type Selector',
-    helpSubTitle: 'Select elements by their type',
-    helpSyntax: 'A',
-    helpPromt: 'Selects all elements of type <strong>A</strong>.Type refers to the type of tag, so <tag>div</tag>, <tag> p</tag> and <tag>ul</tag> are all different element types.',
-    helpList: ['<strong>div</strong> selects all <tag>div</tag> elements.', '<strong>p</strong> selects all <tag>p</tag> elements.'],
-    correctAnswer: 'plate'
+    helpTitle: 'ID Selector',
+    helpSubTitle: 'Select elements with an ID',
+    helpSyntax: '#id',
+    helpPromt: 'Selects the element with a specific <strong>id</strong>. You can also combine the ID selector with the type selector.',
+    helpList: ['<strong>#cool</strong> selects any element with <strong>id="cool"</strong>', '<strong>ul#long</strong> selects <strong>ul id="long"</strong>'],
+    correctAnswer: '#fancy'
   },
 
 ]
 
 export default levels
+
+
+
