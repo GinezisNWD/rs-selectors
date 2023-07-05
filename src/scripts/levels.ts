e356dd92bc138d68430d7cfebc13bac1731eaf70
Ginezis <Ginezis25@gmail.com>
2023-07-05 21:52:18 +0300

    feat: add new 6 lvls for css-dinner-game

--- src/scripts/levels.ts
+++ src/scripts/levels.ts
@@ -66,8 +66,94 @@ const levels: Level[] = [
       '<strong>#fancy</strong><strong>span</strong> selects any <tag>span</tag> elements that are inside of the element with <strong>id="fancy"</strong>'],
     correctAnswer: 'plate apple'
   },
+  {
+    title: 'Select the pickle on the fancy plate',
+    table: '<div class="css-diner__table"><div data-tag="bento1" class="bento"><div data-tag="orange1" class="orange"></div></div><div data-tag="fancy1" class="fancy"><div data-tag="pickle1" class="pickle strobe"></div></div><div data-tag="plate1" class="plate"><div data-tag="pickle2" class="pickle"></div></div>',
+    htmlField: '<div data-tag="bento1">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="orange1">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="fancy1">&nbsp;&nbsp;&lt;plate id="fancy"&gt;</div> <div data-tag="pickle1">&nbsp;&nbsp;&nbsp;&nbsp;&lt;pickle /&gt;</div> <div data-tag="fancy1">&nbsp;&nbsp;&lt;plate /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="pickle2">&nbsp;&nbsp;&nbsp;&nbsp;&lt;pickle /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate /&gt;</div>',
+    curentLevel: 5,
+    isDone: false,
+    helpTitle: '',
+    helpSubTitle: 'Combine the Descendant & ID Selectors',
+    helpSyntax: '#id  A',
+    helpPromt: 'You can combine any selector with the descendent selector.',
+    helpList: ['<strong>#cool span</strong> selects all <tag>span</tag> elements that are inside of elements with <strong>id="cool"</strong>'],
+    correctAnswer: '#fancy pickle'
+  },
+  {
+    title: 'Select the small apples',
+    table: '<div data-tag="apple1" class="apple"></div> <div data-tag="apple2" class="apple small strobe"></div> <div data-tag="plate1" class="plate"> <div data-tag="apple3" class="apple small strobe"></div> </div><div data-tag="plate2" class="plate"></div>',
+    htmlField: '<div data-tag="apple1">&nbsp;&nbsp;&lt;apple /&gt;</div> <div data-tag="apple2">&nbsp;&nbsp;&lt;apple class="small" /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="apple3">&nbsp;&nbsp;&nbsp;&nbsp;&lt;apple class="small" /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate /&gt;</div> <div data-tag="plate2">&nbsp;&nbsp;&lt;plate /&gt;</div>',
+    curentLevel: 6,
+    isDone: false,
+    helpTitle: 'Class Selector',
+    helpSubTitle: 'Select elements by their class',
+    helpSyntax: '.classname',
+    helpPromt: 'The class selector selects all elements with that class attribute. Elements can only have one ID, but many classes.',
+    helpList: ['<strong>.neato</strong> selects all elements with <strong>class="neato"</strong>'],
+    correctAnswer: '.small'
+  },
+  {
+    title: 'Select the small oranges',
+    table: '<div class="css-diner__table"><div data-tag="apple1" class="apple"></div> <div data-tag="apple2" class="apple small"></div> <div data-tag="bento1" class="bento"> <div data-tag="orange1" class="orange small strobe"></div></div> <div data-tag="plate1" class="plate"> <div data-tag="orange2" class="orange"></div></div> <div data-tag="plate2" class="plate"> <div data-tag="orange3" class="orange small strobe"></div> </div>',
+    htmlField: '<div data-tag="apple1">&nbsp;&nbsp;&lt;apple /&gt;</div> <div data-tag="apple2">&nbsp;&nbsp;&lt;apple class="small" /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="orange1">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange class="small" /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="orange2">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate /&gt;</div> <div data-tag="plate2">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="orange3">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange class="small" /&gt;</div><div data-tag="plate2">&nbsp;&nbsp;&lt;plate /&gt;</div>',
+    curentLevel: 7,
+    isDone: false,
+    helpTitle: '',
+    helpSubTitle: 'Combine the Class Selector',
+    helpSyntax: 'A.className',
+    helpPromt: 'You can combine the class selector with other selectors, like the type selector..',
+    helpList: ['<strong>ul.important</strong> selects all <tag>ul</tag> elements that have <strong>class="important"</strong>',
+      '<strong>#big.wide</strong> selects all elements with <strong>id="big"</strong> that also have <strong>class="wide"</strong>'],
+    correctAnswer: 'orange.small'
+  },
+  {
+    title: 'Select the small oranges in the bentos',
+    table: '<div data-tag="bento1" class="bento"> <div data-tag="orange1" class="orange"></div></div> <div data-tag="orange2" class="orange small"></div> <div data-tag="bento2" class="bento"> <div data-tag="orange3" class="orange small strobe"></div></div> <div data-tag="bento3" class="bento"> <div data-tag="apple1" class="apple small"></div></div> <div data-tag="bento4" class="bento"> <div data-tag="orange4" class="orange small strobe"></div> </div>',
+    htmlField: '<div data-tag="bento1">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="orange1">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="orange2">&nbsp;&nbsp;&lt;orange class="small" /&gt;</div> <div data-tag="bento2">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="orange3">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange class="small" /&gt;</div> <div data-tag="bento2">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="bento3">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="apple1">&nbsp;&nbsp;&nbsp;&nbsp;&lt;apple class="small" /&gt;</div> <div data-tag="bento3">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="bento4">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="orange4">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange class="small" /&gt;</div> <div data-tag="bento4">&nbsp;&nbsp;&lt;bento /&gt;</div>',
+    curentLevel: 8,
+    isDone: false,
+    helpTitle: '',
+    helpSubTitle: 'You can do it...',
+    helpSyntax: 'Put your back into it!',
+    helpPromt: 'Combine what you learned in the last few levels to solve this one!',
+    helpList: [''],
+    correctAnswer: 'bento orange.small'
+  },
+  {
+    title: 'Select all the plates and bentos',
+    table: '<div data-tag="pickle1" class="pickle"></div> <div data-tag="plate1" class="plate strobe"> <div data-tag="pickle2" class="pickle strobe"></div> </div> <div data-tag="bento1" class="bento strobe"> <div data-tag="pickle3" class="pickle strobe"></div> </div> <div data-tag="plate2" class="plate strobe"> <div data-tag="pickle4" class="pickle strobe"></div> </div> <div data-tag="pickle5" class="pickle"></div>',
+    htmlField: '<div data-tag="pickle1">&nbsp;&nbsp;&lt;pickle /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="pickle2">&nbsp;&nbsp;&nbsp;&nbsp;&lt;pickle /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="pickle3">&nbsp;&nbsp;&nbsp;&nbsp;&lt;pickle /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="plate2">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="pickle4">&nbsp;&nbsp;&nbsp;&nbsp;&lt;pickle /&gt;</div> <div data-tag="plate2">&nbsp;&nbsp;&lt;plate /&gt;</div> <div data-tag="pickle5">&nbsp;&nbsp;&lt;pickle /&gt;</div>',
+    curentLevel: 9,
+    isDone: false,
+    helpTitle: 'Comma Combinator',
+    helpSubTitle: 'Combine, selectors, with... commas!',
+    helpSyntax: 'A, B',
+    helpPromt: 'Thanks to Shatner technology, this selects all <strong>A</strong> and <strong>B</strong> elements. You can combine any selectors this way, and you can specify more than two.',
+    helpList: ['<strong>p, .fun</strong> selects all <tag>p</tag> elements as well as all elements with <strong>class="fun"</strong>',
+      '<strong>a, p, div</strong> selects all <tag>a</tag>, <tag>p</tag> and <tag>div</tag> elements'],
+    correctAnswer: 'plate, bento'
+  },
+  {
+    title: 'Select all the things!',
+    table: '<div data-tag="apple1" class="apple strobe"></div> <div data-tag="plate1" class="plate strobe"> <div data-tag="orange1" class="orange small strobe"></div> </div> <div data-tag="bento1" class="bento strobe"></div> <div data-tag="bento2" class="bento strobe"> <div data-tag="orange2" class="orange strobe"></div> </div> <div data-tag="fancy1" class="fancy strobe"></div>',
+    htmlField: '<div data-tag="apple1">&nbsp;&nbsp;&lt;apple /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate&gt;</div> <div data-tag="orange1">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange class="small" /&gt;</div> <div data-tag="plate1">&nbsp;&nbsp;&lt;plate /&gt;</div> <div data-tag="bento1">&nbsp;&nbsp;&lt;bento /&gt;</div><div data-tag="bento2">&nbsp;&nbsp;&lt;bento&gt;</div> <div data-tag="orange2">&nbsp;&nbsp;&nbsp;&nbsp;&lt;orange /&gt;</div> <div data-tag="bento2">&nbsp;&nbsp;&lt;bento /&gt;</div> <div data-tag="fancy1">&nbsp;&nbsp;&lt;plate id="fancy" /&gt;</div>',
+    curentLevel: 10,
+    isDone: false,
+    helpTitle: 'The Universal Selector',
+    helpSubTitle: 'You can select everything!',
+    helpSyntax: '*',
+    helpPromt: 'You can select all elements with the universal selector!',
+    helpList: ['<strong>p *</strong> selects any element inside all <tag>p</tag> elements.'],
+    correctAnswer: '*'
+  },
 
 ]
+
+
+
+
+
+
 
 export default levels
 
